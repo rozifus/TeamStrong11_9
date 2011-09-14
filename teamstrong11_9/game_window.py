0ec73ccfbec77398e1f462f748fcfd8fd917e459
Jervis <jervis@whit.(none)>
2011-09-14 21:42:18 +1000

    added a basic menu.. Also broke the game. There is an error now about the MenuOption not having left or right handler. the Quit on the menu doesn't work either.

--- teamstrong11_9/game_window.py
+++ teamstrong11_9/game_window.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from functools import partial
 
 import pyglet
 
@@ -9,18 +10,22 @@ import data
 import settings
 from shortcuts import *
 
-# some dodgy code to call the 'update' function on every sprite
-# every frame.
-
-"""
-def update(dt):
-    map(lambda s: s.update(dt), sprites)
-
-pyglet.clock.schedule_interval(update, 1/30.)
-"""
+import kytten
 
 #----------------------------------------------
 # main game window. Checks for all of the keyboard events atm.
+
+def show_help(window, batch, group, theme):
+
+    document = pyglet.text.decode_attributed(
+            'There is no {bold True}help{bold False}\n\n'
+            'Only {align "center"}lies{align "left"}\n')
+    dialog = kytten.Dialog(
+            kytten.Frame(
+                kytten.Document(document, width=300, height=150)),
+            window=window, batch=batch, group=group,
+            anchor=kytten.ANCHOR_CENTER,
+            theme=theme, on_escape=lambda dialog:dialog.teardown())
 
 class GameWindow(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
@@ -31,7 +36,53 @@ class GameWindow(pyglet.window.Window):
         # Setup updates to run once per tick
         clock.schedule(self.update)
 
+        #---------------------------------------------------------
+        # MENU.
+
+        # set up a menu system. It is a hack.
+        # don't let anyone else tell you otherwise.
+
+        self.menubatch = pyglet.graphics.Batch()
+        self.menugroup = pyglet.graphics.OrderedGroup(1)
+        self.bggroup = pyglet.graphics.OrderedGroup(0)
+        theme = kytten.Theme(fp('theme'))
+
+        menu_choices = {
+                'Play Game': self.remove_menu_load_level,
+                'Help!'    : partial(show_help, self,
+                                     self.menubatch, self.menugroup, theme),
+                'Quit'     : self.quit,
+        }
+
+        def on_select(choice):
+            menu_choices.get(choice)()
+
+        self.background = load(fp('background.png'))
+
+        dialog = kytten.Dialog(
+                kytten.TitleFrame("Altered Panda",
+                    kytten.VerticalLayout([
+                        kytten.Menu(options=[
+                            "Play Game",
+                            "Help!",
+                            "Quit",],
+                            on_select=on_select),
+                    ])
+                ),
+                window=self, batch=self.menubatch,
+                group=self.menugroup, anchor=kytten.ANCHOR_TOP_LEFT,
+                theme=theme
+        )
+        #self.create_level()
+
+    def remove_menu_load_level(self):
+        self.on_draw = lambda: None
         self.create_level()
+
+    def on_draw(self):
+        self.clear()
+        self.background.blit(0, 0)
+        self.menubatch.draw()
 
     def create_level(self):
         self.level = Level(self)
